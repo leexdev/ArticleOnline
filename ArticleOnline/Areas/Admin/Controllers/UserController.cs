4aec74d4a32d3b1d6e48dd2b7627cefd1d6fc976
lepro83 <72549881+lepro83@users.noreply.github.com>
2023-05-26 09:13:39 +0700

    feat: final project

--- ArticleOnline/Areas/Admin/Controllers/UserController.cs
+++ ArticleOnline/Areas/Admin/Controllers/UserController.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace ArticleOnline.Areas.Admin.Controllers
 {
@@ -150,6 +151,129 @@ namespace ArticleOnline.Areas.Admin.Controllers
             articleService.UpdateUserAdmin(user);
             Session["SuccessMessage"] = "Sửa thành công!";
             return Redirect(Request.UrlReferrer.ToString());
+        }
+
+        [CustomAuthorize(Roles = "user")]
+        public ActionResult ShowArticle(string currentFilter, string searchString, int? page)
+        {
+            var objArticleModel = articleService.GetHomeModel();
+
+            Guid authorId = (Guid)Session["Id"];
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                objArticleModel.ListArticle = articleService.GetArticleSearch(searchString).Where(a => a.AuthorId == authorId).ToList();
+            }
+            else
+            {
+                objArticleModel.ListArticle = articleService.GetArticles().Where(a => a.AuthorId == authorId).ToList();
+            }
+
+            ViewBag.CurrentFilter = searchString;
+            int pageSize = 5;
+            int pageNumber = page ?? 1;
+            var pagedArticles = objArticleModel.ListArticle.Where(a => a.AuthorId == authorId).OrderByDescending(a => a.PublishDate).ToPagedList(pageNumber, pageSize);
+            var pagedArticleModels = new StaticPagedList<ArticleManagementModel>(
+                pagedArticles.Select(a => new ArticleManagementModel
+                {
+                    ListArticle = new List<Article> { a }
+                }),
+                pagedArticles.GetMetaData()
+            );
+            objArticleModel.PagedArticles = pagedArticleModels;
+            return View(objArticleModel);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        [CustomAuthorize(Roles = "user")]
+        public ActionResult CreateArticle(Article article, HttpPostedFileBase ImageUpLoad)
+        {
+            if (ImageUpLoad != null)
+            {
+                article.Id = Guid.NewGuid();
+                string fileName = Path.GetFileNameWithoutExtension(ImageUpLoad.FileName);
+                string extension = Path.GetExtension(ImageUpLoad.FileName);
+                fileName = fileName + "_" + long.Parse(DateTime.Now.ToString("yyyyMMddhhmmss")) + extension;
+                article.Avatar = "/Content/img/" + fileName;
+                ImageUpLoad.SaveAs(Path.Combine(Server.MapPath("~/Content/img/"), fileName));
+                article.PublishDate = DateTime.Now;
+                if (Session["Id"] is Guid authorId)
+                {
+                    article.AuthorId = authorId;
+                }
+                articleService.AddArticle(article);
+                var articles = articleService.GetLatestArticles();
+                var objArticleModel = articleService.GetHomeModel();
+                objArticleModel.ListArticle = articles;
+                Session["SuccessMessage"] = "Thêm thành công!";
+                return RedirectToAction("ShowArticle");
+            }
+
+            return RedirectToAction("ShowArticle");
+        }
+
+        [HttpPost]
+        [CustomAuthorize(Roles = "user")]
+        public ActionResult DeleteArticle(Guid id)
+        {
+            var article = articleService.GetArticle(id);
+
+            if (article == null)
+            {
+                return HttpNotFound();
+            }
+
+            articleService.DeleteArticle(article);
+            return RedirectToAction("ShowArticle");
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        [CustomAuthorize(Roles = "user")]
+        public ActionResult EditArticle(Article article, HttpPostedFileBase ImageUpLoad)
+        {
+            if (ImageUpLoad != null)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(ImageUpLoad.FileName);
+                string extension = Path.GetExtension(ImageUpLoad.FileName);
+                fileName = fileName + "_" + long.Parse(DateTime.Now.ToString("yyyyMMddhhmmss")) + extension;
+                article.Avatar = "/Content/img/" + fileName;
+                ImageUpLoad.SaveAs(Path.Combine(Server.MapPath("~/Content/img/"), fileName));
+            }
+            articleService.UpdateArticle(article);
+            Session["SuccessMessage"] = "Sửa thành công!";
+            return RedirectToAction("ShowArticle");
+        }
+
+        [HttpPost]
+        [CustomAuthorize(Roles = "user")]
+        public ActionResult UploadImage(HttpPostedFileBase upload)
+        {
+            if (upload != null && upload.ContentLength > 0)
+            {
+                // Tạo tên tập tin duy nhất bằng cách sử dụng thời gian và Guid
+                var fileName = DateTime.Now.ToString("yyyyMMddHHmmss") + "_" + Guid.NewGuid().ToString("N") + Path.GetExtension(upload.FileName);
+                var imagePath = Path.Combine(Server.MapPath("~/Content/img/"), fileName);
+                upload.SaveAs(imagePath);
+
+                // Trả về URL của hình ảnh đã tải lên
+                var imageUrl = Url.Content("~/Content/img/" + fileName);
+                return Content("<script>window.parent.CKEDITOR.tools.callFunction(" + Request.QueryString["CKEditorFuncNum"] + ", '" + imageUrl + "');</script>");
+            }
+
+            return HttpNotFound();
         }
     }
 }
